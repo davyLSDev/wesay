55dc8c74cbc96267be255c301c892eeca04f1149
Keith Stribley <devel@thanlwinsoft.org>
2010-03-12 19:13:46 +0630

    HTML renderer

---
+++ src/LexicalTools/HtmlRenderer.cs
@@ -0,0 +1,84 @@
+
+using System;
+using System.IO;
+using System.Text;
+using Palaso.DictionaryServices.Model;
+using Palaso.Lift;
+using Palaso.DictionaryServices.Lift;
+using Palaso.Lift.Options;
+using Palaso.Text;
+using System.Xml;
+using System.Xml.Xsl;
+using WeSay.LexicalModel;
+using WeSay.LexicalModel.Foundation;
+using WeSay.Project;
+using WeSay.UI;
+using WeSay.LexicalTools.Properties;
+
+namespace WeSay.LexicalTools
+{
+
+
+	public static class HtmlRenderer
+	{
+		private static XslCompiledTransform transform = null;
+		private static XmlReaderSettings readerSettings = null;
+
+		public static string ToHtml(LexEntry entry,
+								   CurrentItemEventArgs currentItem,
+								   LexEntryRepository lexEntryRepository,
+									ViewTemplate viewTemplate)
+		{
+			string html = "";
+			StringBuilder builder = new StringBuilder();
+			LiftWriter writer = new LiftWriter(builder, true);
+			writer.Add(entry);
+			if (transform == null) initTransform();
+			string xml = builder.ToString();
+			System.IO.StringReader entryReader = new System.IO.StringReader(xml);
+			XsltArgumentList xsltArgs = new XsltArgumentList();
+			xsltArgs.AddParam("extraStyles", "", getWritingSystemStyles(viewTemplate));
+			XmlTextReader entryXmlReader = new XmlTextReader(entryReader);
+			TextWriter htmlTextWriter = new StringWriter();
+			XmlWriter htmlWriter = new XmlTextWriter(htmlTextWriter);
+			transform.Transform(entryXmlReader, xsltArgs, htmlWriter);
+			html = htmlTextWriter.ToString();
+#if DEBUG
+			// temp hack
+			System.IO.File.WriteAllText("/tmp/LexicalEntry.xml", xml);
+			System.IO.File.WriteAllText("/tmp/LexicalEntry.html", html);
+#endif
+			return html;
+		}
+
+		private static string getWritingSystemStyles(ViewTemplate viewTemplate)
+		{
+			System.Text.StringBuilder builder = new System.Text.StringBuilder();
+			foreach (WritingSystem ws in viewTemplate.WritingSystems.Values)
+			{
+				builder.Append(":lang(");
+				builder.Append(ws.Abbreviation);
+				builder.Append(") { font-family: \"");
+				builder.Append(ws.FontName);
+				builder.Append("\"; font-size: ");
+				builder.Append(ws.FontSize);
+				builder.Append("pt;");
+				builder.Append("}");
+			}
+			return builder.ToString();
+		}
+
+		private static void initTransform()
+		{
+			transform = new XslCompiledTransform();
+			readerSettings = new XmlReaderSettings();
+			System.IO.StringReader sr =
+					   new System.IO.StringReader(Resources.liftEntry2Html);
+			XmlReader xsltReader = XmlReader.Create(sr, readerSettings);
+			XsltSettings settings = new XsltSettings(true, true);
+			transform.Load(xsltReader, settings, new XmlUrlResolver());
+			xsltReader.Close();
+		}
+
+	}
+}
